96158b50b7f820a540803f565b07750f7684b85d
Baby <89675173+Swag666baby@users.noreply.github.com>
2024-01-20 18:46:25 -0300

    Update antilink.ts
    Fix antilink

--- src/commands/basic/antilink.ts
+++ src/commands/basic/antilink.ts
@@ -7,8 +7,8 @@ export const antilink = async(message, sock, jid, msg, userNumber, number) => {
     const links = JSON.parse(readFileSync("./database/json/groups/links.json").toString())
     const hasLink = links.some(link => message?.includes(link));
     const userAdmin = await isAdmin(jid, sock, number)
-    const groupMetadata = await sock.groupMetadata(jid)
-    const groupData = await findGroups(groupMetadata) 
+    //const groupMetadata = await sock.groupMetadata(jid)
+    const groupData = await findGroups(jid 
     
     if(hasLink && groupData?.data?.antilink && !userAdmin && userNumber != process.env.BOT_NUMBER){
         sock.groupParticipantsUpdate(jid, [number], "remove");
